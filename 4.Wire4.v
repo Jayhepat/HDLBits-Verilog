33c236ceb96fa2c8477252965cfa4636792dab4d
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-12-17 13:20:05 +0530

    Create top_module with specified input-output connections
    Defined a top module with three inputs and four outputs that connects inputs to outputs as specified.

---
+++ 4.Wire4.v
@@ -0,0 +1,20 @@
+//Create a module with 3 inputs and 4 outputs that behaves like wires that makes these connections:
+// a -> w
+// b -> x
+// b -> y
+// c -> z
+
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+    
+    assign w = a, 
+           x = b,
+           y = b,
+           z = c;
+
+endmodule
+
+    // If we're certain about the width of each signal, using 
+	// the concatenation operator is equivalent and shorter:
+// assign {w,x,y,z} = {a,b,b,c};
