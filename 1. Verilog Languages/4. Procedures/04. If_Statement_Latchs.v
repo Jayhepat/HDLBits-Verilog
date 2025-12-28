633b213914258ca5fab3aba2426202cfdcf89ddf
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-12-28 14:09:06 +0530

    Create 04. If_Statement_Latchs.v

---
+++ 1. Verilog Languages/4. Procedures/04. If_Statement_Latchs.v
@@ -0,0 +1,26 @@
+
+Latch : https://hdlbits.01xz.net/mw/images/d/d1/Always_if2.png
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
+        else
+            shut_off_computer = 0;
+    end
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+        else
+            keep_driving = 0;
+    end
+
+endmodule
